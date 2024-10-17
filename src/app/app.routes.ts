90b0c40b24856d24721f5dd67820c79744739be3
lautaro <lautaroleonelfernandez3@gmail.com>
2024-10-17 15:23:32 -0300

    Primeras paginas y configuración de router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', 
+		children: [
+			{ path: '', loadComponent: () => import('./routes/dashboard/dashboard.component') },
+			{ path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+    ] 
+  },
+	{ path: 'login', loadComponent: () => import('./routes/login/login.component') },
+	{ path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
